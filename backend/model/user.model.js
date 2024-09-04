93ec48854deae4f7817274aeb7f760619f4900d7
Subhra Das <sdas204221@gmail.com>
2024-09-04 20:27:11 +0530

    feat(backend): add dp field to User model schema

--- backend/model/user.model.js
+++ backend/model/user.model.js
@@ -10,6 +10,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    dp: {
+        type: String, 
+        required: false
+      },
     password:{
         type: String,
         required: true
