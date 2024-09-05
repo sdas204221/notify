03dc4188c1de07b2e2ebee4349a057cc5a052ad6
Subhra Das <sdas204221@gmail.com>
2024-09-05 14:19:53 +0530

    Revert "feat(backend): Add Channel model and Notice schema"
    This reverts commit e1f2b9f2a9037609f8c90371b8e6d4cc49e793ac.

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const connect_db = require("./config/db");
+// const User = require("./model/user.model")
+// const Channel = require("./model/channel.model");
 const channelRoutes = require('./routes/channel.routes');
-const userRoutes=require("./routes/user.routes");
 require("dotenv").config();
 
 
 connect_db();
 const app = express();
 
-app.use(express.json());
+
 
 app.use('/api/channels', channelRoutes);
-app.use('/api/users', userRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
