d48563c3cc9ee67a0c0131f834d6176138faecee
Subhra Das <sdas204221@gmail.com>
2024-09-05 20:42:20 +0530

    feat(backend): add admin login functionality
    Implemented `loginAdmin` function for admin login.
    Added route `/admin/login` to handle admin login requests.

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -146,3 +146,78 @@ exports.loginClient = async (req, res) => {
         });
     }
 }
+
+exports.loginAdmin = async (req, res) => {
+    try {
+        const { user_id, password, email } = req.body;
+        if (!password)
+            return res.status(400).json({
+                success: false,
+                message: 'password is required',
+            });
+
+        if (user_id) {
+            user = await User.findOne({
+                user_id: user_id
+            })
+
+        } else if (email) {
+            user = await User.findOne({
+                email: email
+            })
+
+        } else {
+            return res.status(400).json({
+                success: false,
+                message: 'user id or email is required',
+            });
+        }
+
+        if (!user)
+            return res.status(401).json({
+                success: false,
+                message: 'invalid credentials'
+            });
+            const isPasswordValid = await argon2.verify(user.password, password);
+            if(!isPasswordValid)
+                return res.status(401).json({
+                    success: false,
+                    message: 'invalid credentials'
+                });
+
+
+        i = 0
+        while (i <= 5) {
+            const newToken = generateToken(20)
+            if (!user.session_tokens.includes(newToken)) {
+                user.session_tokens.push(newToken)
+                await user.save();
+                return res.status(200).json({
+                    success: true,
+                    session_token: newToken,
+                    name: user.name,
+                    user_id: user.user_id,
+                    email: user.email,
+                    dp: user.dp,
+                    privileged_channels: user.privileged_channels
+                });
+            }
+            i = i + 1;
+        }
+
+
+
+
+        return res.status(500).json({
+            success: false,
+            message: 'failed to generate a unique session token after multiple attempts'
+        });
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            success: false,
+            message: 'an unexpected error occurred'
+        });
+    }
+}
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -4,4 +4,5 @@ const user_router=express.Router();
 
 user_router.post("/register",userController.register);
 user_router.post("/client/login",userController.loginClient);
+user_router.post("/admin/login",userController.loginAdmin);
 module.exports = user_router;
